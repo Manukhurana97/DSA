9a1ec16bc31f994235a49d4bef5bb8b1034de882
manu <manukhurana10397@gmail.com>
2023-07-29 16:45:50 +0530

    DSA

--- src/main/java/com/example/dsa/NewCode/Arrays3/Arrays1.java
+++ src/main/java/com/example/dsa/NewCode/Arrays3/Arrays1.java
@@ -209,6 +209,50 @@ public class Arrays1 {
         return xor;
     }
 
+
+    public static List<List<Integer>> largestSumSubarray(int[] arr, int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        /*for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                if(sum==k){
+                    List<Integer> list = new ArrayList<>();
+                    for (int z = i; z <= j; z++) list.add(arr[z]);
+                    result.add(list);
+                    break;
+                }
+                else if ((sum + arr[j]) > k) break;
+            }
+
+        }*/
+
+//        int[] arr = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int len = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            if (sum == k) {
+                len = Math.max(len, i + 1);
+            }
+
+            int rem = sum - k;
+            if (map.containsKey(rem)) {
+                int mlen = i - map.get(rem);
+                len = Math.max(len, mlen);
+            }
+            if (!map.containsKey(rem))
+            map.put(sum, i);
+
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
 /*
         System.out.println(largestElement(new int[]{3,2,1,5,2}));
@@ -249,9 +293,15 @@ public class Arrays1 {
 
         int[] arr = {1, 1, 0, 1, 1, 1, 1, 0};
         System.out.println(maxConsecutiveOnce(arr, arr.length));
-*/
 
         int[] arr = {1, 1, 2, 3, 3,4, 4, 5, 5};
         System.out.println(nonDuplicateInDuplicateList(arr));
+*/
+
+        int[] arr = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
+        var data = largestSumSubarray(arr, arr.length, 4);
+        for (var lst : data) {
+            System.out.println(lst);
+        }
     }
 }
