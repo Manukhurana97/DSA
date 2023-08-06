61148e6cd6eb13f6e932d269c6c8469883683a91
manu <manukhurana10397@gmail.com>
2023-08-07 02:42:01 +0530

    dsa

--- src/main/java/com/example/dsa/NewCode/Arrays3/CountSubArraySumEqualsToK13.java
+++ src/main/java/com/example/dsa/NewCode/Arrays3/CountSubArraySumEqualsToK13.java
@@ -28,24 +28,46 @@ public class CountSubArraySumEqualsToK13 {
 
     public static int countSumLen(int[] arr, int k) {
 
+        Map<Integer, Integer> map = new HashMap<>();
+
         int count = 0;
-        int sum = 0;
+        int presum = 0;
+        map.put(0, 1);
+
+        for (int i = 0; i < arr.length; i++) {
+            presum += arr[i];
+
+            if (map.containsKey(presum - k)) {
+                count += map.get(presum - k);
+            }
+            map.put(presum, map.getOrDefault(presum, 0) + 1);
+        }
+        return count;
+    }
+    public static int countSumLen1(int[] arr, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int count = 0;
+        int presum = 0;
         int n = arr.length;
 
+        map.put(0, 1);
+
         for (int i = 0; i < n; i++) {
-            sum = 0;
-            for (int j = i; j < n; j++) {
-                sum += arr[j];
-                if (sum == k) {
-                    count += 1;
-                }
+
+            presum += arr[i];
+
+            if (map.containsKey(presum - k)) {
+                count += map.get(presum - k);
             }
+
+            map.put(presum, map.getOrDefault(presum, 0) + 1);
+
         }
         return count;
     }
 
-
-    public static int countSumLen1(int[] arr, int k) {
+    public static int countSumLen2(int[] arr, int k) {
         Map<Integer, Integer> map = new HashMap<>();
 
         int count = 0;
@@ -56,7 +78,7 @@ public class CountSubArraySumEqualsToK13 {
 
             presum += arr[i];
 
-            count += (presum == k) ? 1 : 0;
+            count += ((presum ^ k) == 0) ? 1 : 0;
             count += map.getOrDefault(presum - k, 0);
 
             map.put(presum, map.getOrDefault(presum, 0) + 1);
@@ -67,14 +89,16 @@ public class CountSubArraySumEqualsToK13 {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, -3, 1, 1, 1, 4, 2, -3};
-        /*  var result = countSumValue(arr, 3);
-        var result1 = countSumLen(arr, 3);
-        var result2 = countSumLen1(arr, 0);*/
+//          var result = countSumValue(arr, 3);
+//        var result1 = countSumLen(arr, 3);
+        var result2 = countSumLen1(arr, 3);
+//        var result3 = countSumLen2(arr, 3);
+        System.out.println(result2);
 
 
-        int[] arr1 = {1};
-        var result3 = countSumLen1(arr1, 0);
-        System.out.println(result3);
+//        int[] arr1 = {1};
+//        var result3 = countSumLen1(arr1, 0);
+//        System.out.println(result3);
 
     }
 }
