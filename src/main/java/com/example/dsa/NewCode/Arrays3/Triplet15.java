971d5ce6ec03ccff28c19d7b2252f3ab68826b8f
manu <manukhurana10397@gmail.com>
2023-08-13 18:20:13 +0530

    dsa

--- src/main/java/com/example/dsa/NewCode/Arrays3/Triplet15.java
+++ src/main/java/com/example/dsa/NewCode/Arrays3/Triplet15.java
@@ -46,9 +46,51 @@ public class Triplet15 {
     }
 
 
+    public static List<List<Integer>> getTriplet2(int[] arr) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = arr.length;
+
+        Arrays.sort(arr);
+
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum == 0) {
+                    result.add(Arrays.asList(arr[i], arr[j], arr[k]));
+
+                    while (j < k && arr[j] == arr[j + 1]) {
+                        j++;
+                    }
+                    while (j < k && arr[k] == arr[k - 1]) {
+                        k--;
+                    }
+
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+
+        return result;
+
+    }
+
+
     public static void main(String[] args) {
         int[] arr = {-1, 0, 1, 2, -1, -4};
-        Set<List<Integer>> result = getTriplet1(arr);
+        var result = getTriplet2(arr);
         for (var i : result) {
             System.out.println(i);
         }
