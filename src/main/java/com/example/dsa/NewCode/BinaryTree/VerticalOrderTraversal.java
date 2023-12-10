e4ac8d400b87cb9c21bebcf89004ab9619dce780
manu <manukhurana10397@gmail.com>
2023-12-11 01:06:04 +0530

    vertical traversal

--- src/main/java/com/example/dsa/NewCode/BinaryTree/VerticalOrderTraversal.java
+++ src/main/java/com/example/dsa/NewCode/BinaryTree/VerticalOrderTraversal.java
@@ -106,15 +106,26 @@ public class VerticalOrderTraversal {
     }
 
     public static void main(String[] args) {
+        // Node head = new Node(1);
+        // head.left = new Node(2);
+        // head.right = new Node(3);
+        // head.left.left = new Node(4);
+        // head.left.right = new Node(5);
+        // head.right.left = new Node(6);
+        // head.right.right = new Node(7);
+        // head.right.right.left = new Node(8);
+        // head.right.right.right = new Node(9);
+
+        // System.out.println(verticalOrder(head));
+        // System.out.println(verticalOrder1(head));
+
         Node head = new Node(1);
         head.left = new Node(2);
         head.right = new Node(3);
         head.left.left = new Node(4);
         head.left.right = new Node(5);
-        head.right.left = new Node(6);
+        head.left.right.left = new Node(6);
         head.right.right = new Node(7);
-        head.right.right.left = new Node(8);
-        head.right.right.right = new Node(9);
 
         System.out.println(verticalOrder(head));
         System.out.println(verticalOrder1(head));
