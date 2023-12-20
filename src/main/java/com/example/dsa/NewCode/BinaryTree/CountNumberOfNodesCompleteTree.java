2ce9734de62417b99f04e7e52671243dad543409
manu <manukhurana10397@gmail.com>
2023-12-20 18:24:28 +0530

    CountNumberOfNodesCompleteTree

--- src/main/java/com/example/dsa/NewCode/BinaryTree/CountNumberOfNodesCompleteTree.java
+++ src/main/java/com/example/dsa/NewCode/BinaryTree/CountNumberOfNodesCompleteTree.java
@@ -10,6 +10,41 @@ public class CountNumberOfNodesCompleteTree {
         return 1 + countNodes(head.left) + countNodes(head.right);
     }
 
+    private static int countNodes1(Node head) {
+
+        if (head == null)
+            return 0;
+
+        int leftCount = getLeftHeight(head);
+        int rightCount = getRightHeight(head);
+
+        if (leftCount == rightCount) {
+            return (1 << leftCount) - 1;
+        } else {
+            return 1 + countNodes1(head.left) + countNodes1(head.right);
+        }
+    }
+
+    public static int getLeftHeight(Node head) {
+        int count = 0;
+        while (head != null) {
+            count++;
+            head = head.left;
+        }
+
+        return count;
+    }
+
+    public static int getRightHeight(Node head) {
+        int count = 0;
+        while (head != null) {
+            count++;
+            head = head.right;
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
         Node node = new Node(1);
         node.left = new Node(2);
@@ -20,5 +55,6 @@ public class CountNumberOfNodesCompleteTree {
         node.left.right.right = new Node(7);
 
         System.out.println(countNodes(node));
+        System.out.println(countNodes1(node));
     }
 }
