b875e75835440e62aa1148f69fb747ebe498a68e
manu <manukhurana10397@gmail.com>
2023-12-22 02:22:08 +0530

    SerializeDeserialize

--- src/main/java/com/example/dsa/NewCode/BinaryTree/SerializeDeserialize.java
+++ src/main/java/com/example/dsa/NewCode/BinaryTree/SerializeDeserialize.java
@@ -1,15 +1,42 @@
 package com.example.dsa.NewCode.BinaryTree;
 
+import java.util.*;
+
 public class SerializeDeserialize {
 
     private static String serializer(Node head) {
-        String result = "";
-        return result;
+        StringBuilder result = new StringBuilder();
+
+        if (head == null)
+            return result.toString();
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(head);
+        result.append(head.val).append(",");
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+
+            if (node.left != null) {
+                queue.add(node.left);
+                result.append(node.left.val).append(",");
+            } else {
+                result.append("#").append(",");
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+                result.append(node.right.val).append(",");
+            } else {
+                result.append("#").append(",");
+            }
+        }
+
+        return result.toString();
     }
 
-    private static Node deSerializer(String str) {
-        return null;
-    }
+    // private static Node deSerializer(String str) {
+    // return null;
+    // }
 
     public static void main(String[] args) {
         Node head = new Node(1);
@@ -17,6 +44,8 @@ public class SerializeDeserialize {
         head.right = new Node(13);
         head.right.left = new Node(4);
         head.right.right = new Node(5);
+
+        System.out.println(serializer(head));
 
     }
 
