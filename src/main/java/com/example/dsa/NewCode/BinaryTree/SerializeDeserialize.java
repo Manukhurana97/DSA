75b817c8109d6c49d3dd516868c528de1920b198
manu <manukhurana10397@gmail.com>
2023-12-22 02:13:14 +0530

    SerializeDeserialize

---
+++ src/main/java/com/example/dsa/NewCode/BinaryTree/SerializeDeserialize.java
@@ -0,0 +1,23 @@
+package com.example.dsa.NewCode.BinaryTree;
+
+public class SerializeDeserialize {
+
+    private static String serializer(Node head) {
+        String result = "";
+        return result;
+    }
+
+    private static Node deSerializer(String str) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.left = new Node(2);
+        head.right = new Node(13);
+        head.right.left = new Node(4);
+        head.right.right = new Node(5);
+
+    }
+
+}
