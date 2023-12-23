1abee0fd8e6035ee74074d1acd1792ff1e865fd6
manu <manukhurana10397@gmail.com>
2023-12-24 01:37:47 +0530

    SearchInBST

---
+++ src/main/java/com/example/dsa/NewCode/Tree/BinarySearchTree/Node.java
@@ -0,0 +1,16 @@
+package com.example.dsa.NewCode.Tree.BinarySearchTree;
+
+public class Node {
+
+    int value;
+    Node left;
+    Node right;
+
+    Node() {
+    }
+
+    Node(int value) {
+        this.value = value;
+    }
+
+}
---
+++ src/main/java/com/example/dsa/NewCode/Tree/BinarySearchTree/SearchInBST.java
@@ -0,0 +1,29 @@
+package com.example.dsa.NewCode.Tree.BinarySearchTree;
+
+public class SearchInBST {
+
+    private static Node search(Node head, int value) {
+        if (head == null)
+            return null;
+        if (head.value == value)
+            return head;
+
+        return search((head.value > value) ? head.left : head.right, value);
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(8);
+        head.left = new Node(5);
+        head.left.left = new Node(4);
+        head.left.right = new Node(7);
+        head.left.right.left = new Node(6);
+        head.right = new Node(12);
+        head.right.left = new Node(10);
+        head.right.right = new Node(14);
+        head.right.right.left = new Node(13);
+
+        System.out.println(search(head, 7).value);
+        System.out.println(search(head, 10).value);
+        System.out.println(search(head, 0));
+    }
+}
