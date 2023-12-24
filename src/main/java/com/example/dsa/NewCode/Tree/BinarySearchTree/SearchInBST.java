8c7ffa8efe12c78420b577344aae2961033ee240
manu <manukhurana10397@gmail.com>
2023-12-24 12:54:02 +0530

    SearchInBST

--- src/main/java/com/example/dsa/NewCode/Tree/BinarySearchTree/SearchInBST.java
+++ src/main/java/com/example/dsa/NewCode/Tree/BinarySearchTree/SearchInBST.java
@@ -11,6 +11,14 @@ public class SearchInBST {
         return search((head.value > value) ? head.left : head.right, value);
     }
 
+    private static Node search1(Node head, int value) {
+        while (head != null && head.value != value) {
+            head = (head.value > value) ? head.left : head.right;
+        }
+
+        return head;
+    }
+
     public static void main(String[] args) {
         Node head = new Node(8);
         head.left = new Node(5);
@@ -23,7 +31,10 @@ public class SearchInBST {
         head.right.right.left = new Node(13);
 
         System.out.println(search(head, 7).value);
+        System.out.println(search1(head, 7).value);
         System.out.println(search(head, 10).value);
+        System.out.println(search1(head, 10).value);
         System.out.println(search(head, 0));
+        System.out.println(search1(head, 0));
     }
 }
