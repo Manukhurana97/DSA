5ac6e0c3d57fc58e7d10a773f72e0a8c2fa6e11f
manu <manukhurana10397@gmail.com>
2023-12-30 14:46:24 +0530

    find largest bst in bt

--- src/main/java/com/example/dsa/NewCode/Tree/BinarySearchTree/findLargestBST.java
+++ src/main/java/com/example/dsa/NewCode/Tree/BinarySearchTree/findLargestBST.java
@@ -1,5 +1,17 @@
 package com.example.dsa.NewCode.Tree.BinarySearchTree;
 
+class Result {
+    int size;
+    int min;
+    int max;
+
+    Result(int size, int min, int max) {
+        this.size = size;
+        this.min = min;
+        this.max = max;
+    }
+}
+
 public class findLargestBST {
 
     // iterate each node
@@ -46,6 +58,32 @@ public class findLargestBST {
         return 1 + countNodes(node.left) + countNodes(node.right);
     }
 
+    /*------------------------------------------------------ CASE 2 -----------------------------------------------------------------*/
+
+    private static int largestBST1(Node node) {
+        if (node == null)
+            return 0;
+
+        return largestBST1Helper(node).size;
+    }
+
+    private static Result largestBST1Helper(Node node) {
+        if (node == null) {
+            return new Result(0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+
+        Result lr = largestBST1Helper(node.left);
+        Result rr = largestBST1Helper(node.right);
+
+        // if a validate bst
+        if (lr.max < node.value && node.value < rr.min) {
+            return new Result((1 + lr.size + rr.size), Math.min(node.value, lr.min), Math.max(node.value, rr.max));
+        }
+        // if current tree is not BST
+        return new Result(Math.max(lr.size, rr.size), Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+    }
+
     public static void main(String[] args) {
         Node node = new Node(10);
         node.left = new Node(5);
@@ -55,5 +93,6 @@ public class findLargestBST {
         node.right.right = new Node(7);
 
         System.out.println(largestBST(node));
+        System.out.println(largestBST1(node));
     }
 }
