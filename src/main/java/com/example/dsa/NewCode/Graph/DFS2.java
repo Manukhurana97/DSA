adf54dc4eb3cbf8fea165b985f0e09748b4731e1
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2024-01-23 16:51:59 +0530

    graph/dfs

--- src/main/java/com/example/dsa/NewCode/Graph/DFS2.java
+++ src/main/java/com/example/dsa/NewCode/Graph/DFS2.java
@@ -11,12 +11,43 @@ public class DFS2 {
         stack.push(v - 1);
         visited.add(v - 1);
 
-        helper(input, v, result, visited, stack);
+        dfs(input, v, result, visited, stack);
 
         return result;
     }
 
-    private static void helper(List<List<Integer>> input, int v, List<Integer> result, Set<Integer> visited,
+    private static void dfs(List<List<Integer>> input, int v, List<Integer> result, Set<Integer> visited,
+            Stack<Integer> stack) {
+
+        while (!stack.isEmpty()) {
+            var vertex = stack.pop();
+            result.add(vertex + 1);
+
+            var neighbours = input.get(vertex);
+            for (var neighbour : neighbours) {
+                if (!visited.contains(neighbour - 1)) {
+                    stack.add(neighbour - 1);
+                    visited.add(neighbour - 1);
+                    dfs(input, neighbour - 1, result, visited, stack);
+                }
+            }
+        }
+    }
+
+    /******************************************************************************************************************************************/
+
+    private static List<Integer> triverse1(List<List<Integer>> input, int v) {
+        List<Integer> result = new ArrayList<>();
+        Set<Integer> visited = new HashSet<>();
+        Stack<Integer> stack = new Stack<>();
+        stack.push(v - 1);
+
+        dfs1(input, v, result, visited, stack);
+
+        return result;
+    }
+
+    private static void dfs1(List<List<Integer>> input, int v, List<Integer> result, Set<Integer> visited,
             Stack<Integer> stack) {
 
         while (!stack.isEmpty()) {
@@ -28,7 +59,7 @@ public class DFS2 {
                 if (!visited.contains(node - 1)) {
                     stack.add(node - 1);
                     visited.add(node - 1);
-                    helper(input, node - 1, result, visited, stack);
+                    dfs1(input, node - 1, result, visited, stack);
                 }
             }
         }
@@ -46,7 +77,8 @@ public class DFS2 {
         input.add(Arrays.asList(7));
         input.add(Arrays.asList(6));
 
-        System.out.println(triverse(input, 1));
+        System.out.println("a " + triverse(input, 1));
+        System.out.println("b " + triverse1(input, 1));
     }
 
 }
