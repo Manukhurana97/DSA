2cb3b022ef4fed61a40ae3ae954cfa7be913bcb2
manu <manukhurana10397@gmail.com>
2024-01-25 01:00:31 +0530

    graph/detect cycle in undirected graph

---
+++ src/main/java/com/example/dsa/NewCode/Graph/DetechGraphIsCyclic.java
@@ -0,0 +1,50 @@
+package com.example.dsa.NewCode.Graph;
+
+import java.util.*;
+
+public class DetechGraphIsCyclic {
+
+    private static boolean isGraphCyclic(int v, List<List<Integer>> list) {
+
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        Map<Integer, Integer> parent = new HashMap<>(); // current node, who is the parent
+
+        for (int i = 0; i < v; i++) { // for all the nodes
+            if (!visited.contains(i)) { // if not visited
+                queue.add(i); // add to the queue
+                parent.put(i, null);
+
+                while (!queue.isEmpty()) {
+                    int current = queue.poll(); // get current elemnt from queue
+                    visited.add(current); // mark as visited
+
+                    for (int neighbour : list.get(current)) { // get all the neighbours of current
+                        if (!visited.contains(neighbour)) { // if not visited , the add to the queue and get the parents
+                            queue.add(neighbour);
+                            parent.put(neighbour, current);
+                        } else if (parent.get(neighbour) != current) // if already visited, the check if parent is same
+                                                                     // as current ot not
+                            return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> input = new ArrayList<>();
+        input.add(Arrays.asList(2, 6));
+        input.add(Arrays.asList(3, 1));
+        input.add(Arrays.asList(2, 4));
+        input.add(Arrays.asList(3, 5));
+        input.add(Arrays.asList(4, 6));
+        input.add(Arrays.asList(1, 5, 7));
+        input.add(Arrays.asList(6));
+
+        System.out.println(isGraphCyclic(1, input));
+    }
+
+}
