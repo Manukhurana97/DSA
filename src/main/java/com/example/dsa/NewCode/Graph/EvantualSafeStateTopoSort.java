80b4ba88f625619e4d2f3f39c69437894c96d6f9
manu <manukhurana10397@gmail.com>
2024-01-28 15:21:19 +0530

    eventual safe node, topo sort

--- src/main/java/com/example/dsa/NewCode/Graph/EvantualSafeStateTopoSort.java
+++ src/main/java/com/example/dsa/NewCode/Graph/EvantualSafeStateTopoSort.java
@@ -1,11 +1,12 @@
 package com.example.dsa.NewCode.Graph;
 
-import java.util.*;
 
 /* convert all inbound nodes into outbound notes 
     perform a topo sort,
     all the nodes that lead/points to exit node will come in result, and it a cycle exist it will skip all the nodes as it can never have indegree as 0
  */
+
+import java.util.*;
 
 public class EvantualSafeStateTopoSort {
 
@@ -55,17 +56,17 @@ public class EvantualSafeStateTopoSort {
 
     public static void main(String[] args) {
         List<List<Integer>> input = new ArrayList<>();
-        input.add(Arrays.asList(1));
-        input.add(Arrays.asList(2));
+        input.add(List.of(1));
+        input.add(List.of(2));
         input.add(Arrays.asList(3, 4));
         input.add(Arrays.asList(4, 5));
-        input.add(Arrays.asList(6));
-        input.add(Arrays.asList(6));
-        input.add(Arrays.asList(0));
+        input.add(List.of(6));
+        input.add(List.of(6));
+        input.add(List.of(0));
         input.add(Arrays.asList(1, 9));
-        input.add(Arrays.asList(10));
-        input.add(Arrays.asList(8));
-        input.add(Arrays.asList(9));
+        input.add(List.of(10));
+        input.add(List.of(8));
+        input.add(List.of(9));
 
         System.out.println(eventualSafeNodes(input));
     }
