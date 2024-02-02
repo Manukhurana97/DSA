10dc40e376a33235f901b0e5ba20c4d91e97f22f
manu <manukhurana10397@gmail.com>
2024-02-02 16:29:31 +0530

    graph/ find sorted path from source to destination, dijkstra

--- src/main/java/com/example/dsa/NewCode/Graph/PrintShortestPathUsingDijkastraAlgo.java
+++ src/main/java/com/example/dsa/NewCode/Graph/PrintShortestPathUsingDijkastraAlgo.java
@@ -21,6 +21,8 @@ public class PrintShortestPathUsingDijkastraAlgo {
 
     }
 
+    // in the node add the list to store the parent hierarchy and perform the
+    // dijkstra also
     private static List<Integer> getShortestPath(int start, int end, List<List<List<Integer>>> adj) {
 
         PriorityQueue<Node> queue = new PriorityQueue<>(Comparator.comparing(n -> n.distanceTravelledTillNow));
@@ -41,6 +43,7 @@ public class PrintShortestPathUsingDijkastraAlgo {
                 if (distance[neighbor] > (currentNode.distanceTravelledTillNow + neighborDistance)) {
                     List<Integer> nodesTravelTillNow = new ArrayList<>(currentNode.nodesTravelTillNow);
                     nodesTravelTillNow.add(neighbor);
+
                     if (neighbor == end)
                         result = nodesTravelTillNow;
 
@@ -52,7 +55,53 @@ public class PrintShortestPathUsingDijkastraAlgo {
         }
 
         return result.isEmpty() ? Arrays.asList(-1) : result;
+    }
 
+    // ------------------------------------------------------------------
+
+    /*
+     * create a parent array to keep the track of current node parent, and perform
+     * the dijkestra algo
+     */
+    private static List<Integer> getShortestPath1(int start, int end, List<List<List<Integer>>> adj) {
+
+        int[] parent = new int[adj.size()];
+        parent[start] = start;
+
+        int[] distance = new int[adj.size()];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[start] = 0;
+
+        PriorityQueue<Node> queue = new PriorityQueue<>(Comparator.comparing(n -> n.distanceTravelledTillNow));
+        queue.add(new Node(start, 0));
+
+        while (!queue.isEmpty()) {
+            var node = queue.poll();
+            var currentPoint = node.currentNode;
+            var distanceTravelledTillNow = node.distanceTravelledTillNow;
+
+            for (int i = 0; i < adj.get(currentPoint).size(); i++) {
+                var neighbour = adj.get(currentPoint).get(i).get(0);
+                var neighbourDiatance = adj.get(currentPoint).get(i).get(1);
+
+                if (distance[neighbour] > distanceTravelledTillNow + neighbourDiatance) {
+                    parent[neighbour] = currentPoint; // add parent of neighbours
+                    distance[neighbour] = distanceTravelledTillNow + neighbourDiatance;
+                    queue.add(new Node(neighbour, (distanceTravelledTillNow + neighbourDiatance)));
+                }
+            }
+        }
+        List<Integer> result = new LinkedList<>();
+        result.add(end);
+        for (int i = 0; i < parent.length; i++) {
+            end = parent[end];
+            result.add(0, end);
+            if (end == start)
+                return result;
+
+        }
+
+        return Arrays.asList(-1);
     }
 
     // ------------------------------ input -----------------------------
@@ -86,6 +135,7 @@ public class PrintShortestPathUsingDijkastraAlgo {
         addEdge(adjacencyList, 6, 7, 1);
 
         System.out.println(getShortestPath(startNode, endNode, adjacencyList));
+        System.out.println(getShortestPath1(startNode, endNode, adjacencyList));
 
     }
 }
