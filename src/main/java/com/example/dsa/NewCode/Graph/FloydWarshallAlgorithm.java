4e562873f422db6ff4b78449376ad936b043d363
manu <manukhurana10397@gmail.com>
2024-02-10 18:36:18 +0530

    graph/ floyd warshall algo

---
+++ src/main/java/com/example/dsa/NewCode/Graph/FloydWarshallAlgorithm.java
@@ -0,0 +1,36 @@
+package com.example.dsa.NewCode.Graph;
+
+public class FloydWarshallAlgorithm {
+
+    public void shortestDiatance(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == -1) {
+                    arr[i][j] = Integer.MAX_VALUE;
+                }
+                if (i == j) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+
+        /* relaxing fields */
+        for (int k = 0; k < arr.length; k++) {
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    if (arr[i][k] != Integer.MAX_VALUE && arr[k][j] == Integer.MAX_VALUE)
+                        arr[i][j] = Math.max(arr[i][j], arr[i][k] + arr[k][j]);
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == Integer.MAX_VALUE) {
+                    arr[i][j] = -1;
+                }
+            }
+        }
+    }
+
+}
