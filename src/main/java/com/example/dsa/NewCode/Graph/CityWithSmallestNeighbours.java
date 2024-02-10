45cbf2220f0ce764e15d04c5bdf35cbf78babdc3
manu <manukhurana10397@gmail.com>
2024-02-10 19:48:53 +0530

    graph/ floyd warshall algo

---
+++ src/main/java/com/example/dsa/NewCode/Graph/CityWithSmallestNeighbours.java
@@ -0,0 +1,64 @@
+package com.example.dsa.NewCode.Graph;
+
+public class CityWithSmallestNeighbours {
+
+    /* find the max city wih minimum number of neighbours */
+    private static int findCity(int n, int[][] edges, int thrashold) {
+
+        int[][] distance = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                distance[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        for (var node : edges) {
+            distance[node[0]][node[1]] = node[2];
+            distance[node[1]][node[0]] = node[2];
+        }
+
+        for (int i = 0; i < n; i++) {
+            distance[i][i] = 0;
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (distance[i][k] != Integer.MAX_VALUE || distance[k][j] != Integer.MAX_VALUE)
+                        distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
+                }
+            }
+        }
+
+        int cityNo = n;
+        int cityCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                count += (distance[i][j] <= thrashold) ? 1 : 0;
+            }
+
+            if (cityCount >= count) {
+                cityNo = i;
+                cityCount = count;
+            }
+        }
+
+        return cityNo;
+    }
+
+    public static void main(String[] args) {
+        int[][] edges = {
+                { 0, 1, 10 },
+                { 0, 2, 20 },
+                { 1, 3, 15 },
+                { 2, 4, 25 },
+                { 3, 4, 5 }
+        };
+
+        System.out.println(findCity(5, edges, 30));
+    }
+
+}
