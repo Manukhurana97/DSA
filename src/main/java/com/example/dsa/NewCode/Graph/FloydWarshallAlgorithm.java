333432f35428d1a4500703083e41fc65f92743bc
manu <manukhurana10397@gmail.com>
2024-02-19 16:00:46 +0530

    graph/ floyd warshall algo

--- src/main/java/com/example/dsa/NewCode/Graph/FloydWarshallAlgorithm.java
+++ src/main/java/com/example/dsa/NewCode/Graph/FloydWarshallAlgorithm.java
@@ -1,5 +1,12 @@
 package com.example.dsa.NewCode.Graph;
 
+/* 
+ * make all the nodes -1 to inf and i==i  to 0
+ * take 3 for loop , i, j, k
+ * if j->i && i -> k not inf
+ *  update the distance of j-k> by math.max(j->k,  j->i + i->k);
+ * make back all inf node to -1
+ */
 public class FloydWarshallAlgorithm {
 
     public void shortestDiatance(int[][] arr) {
@@ -30,6 +37,25 @@ public class FloydWarshallAlgorithm {
                     arr[i][j] = -1;
                 }
             }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] graph = {
+                { 0, 3, -1, 7 },
+                { 8, 0, 2, -1 },
+                { 5, -1, 0, 1 },
+                { -1, -1, -1, 0 }
+        };
+
+        FloydWarshallAlgorithm floydWarshall = new FloydWarshallAlgorithm();
+        floydWarshall.shortestDiatance(graph);
+
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph[i].length; j++) {
+                System.out.print(graph[i][j] + "\t");
+            }
+            System.out.println();
         }
     }
 
