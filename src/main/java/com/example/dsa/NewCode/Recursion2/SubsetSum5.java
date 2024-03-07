d1ad636832f33774f608d9746a954416bbffbb73
manu <manukhurana10397@gmail.com>
2024-03-08 01:43:28 +0530

    Recurssion/ subset of list

--- src/main/java/com/example/dsa/NewCode/Recursion2/SubsetSum5.java
+++ src/main/java/com/example/dsa/NewCode/Recursion2/SubsetSum5.java
@@ -8,18 +8,24 @@ public class SubsetSum5 {
 
     private void findSubset(int ind, int[] arr, ArrayList<Integer> lst, List<List<Integer>> listList) {
 
-            if(ind == arr.length){
-                return;
-            }
+        if (ind == arr.length) {
+            listList.add(new ArrayList<>(lst));
+            return;
+        }
 
         for (int i = ind; i < arr.length; i++) {
-            if (i != ind && arr[i] == arr[i - 1]) continue;
+            if (i != ind && arr[i] == arr[i - 1])
+                continue;
 
-            lst.add(arr[i]);
-            listList.add(new ArrayList<>(lst));
-            findSubset(i+1, arr, lst, listList);
-            lst.remove(lst.size() - 1);
+            lst.add(arr[i]); // take the element
+            findSubset(i + 1, arr, lst, listList);
+            lst.remove(lst.size() - 1); // not taking the element
         }
+
+        // lst.add(arr[ind]); // take the element
+        // findSubset(ind + 1, arr, lst, listList);
+        // lst.remove(lst.size() - 1); // not taking the element
+        // findSubset(ind + 1, arr, lst, listList);
     }
 
     private void subsetWithoutDuplicate(int[] arr) {
@@ -29,20 +35,17 @@ public class SubsetSum5 {
         findSubset(0, arr, new ArrayList<>(), listList);
 
         System.out.println(listList.size());
-        for(var i: listList){
+        for (var i : listList) {
             System.out.println(i);
         }
     }
 
-
     public static void main(String[] args) {
         SubsetSum5 subsetSum5 = new SubsetSum5();
 
-//        int[] arr = {1,2,2,2,3,3};
-        int[] arr = {3,1,2,4};
+        // int[] arr = {1,2,2,2,3,3};
+        int[] arr = { 3, 1, 2 };
         subsetSum5.subsetWithoutDuplicate(arr);
     }
 
-
 }
-
