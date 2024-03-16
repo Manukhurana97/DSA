0374f92755cc4e9e0de2cfc16666ac96fbc61857
manu <manukhurana10397@gmail.com>
2024-03-16 17:39:34 +0530

    dp/ subset

--- src/main/java/com/example/dsa/NewCode/DP/SubSetSumEqualToTarget.java
+++ src/main/java/com/example/dsa/NewCode/DP/SubSetSumEqualToTarget.java
@@ -100,4 +100,35 @@ public class SubSetSumEqualToTarget {
         return prev[target];
     }
 
+    private int minimumGenerateAllSubsetsTabulation(int n, int target, int[] arr) {
+        if (target < 0)
+            return -target;
+
+        boolean[][] dp = new boolean[arr.length + 1][target + 1];
+
+        for (int i = 0; i < n; i++) // it means that it's possible to achieve a sum of "0" with the subset
+            dp[i][0] = true;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= target; j++) { // target
+                boolean take = false;
+                if (arr[i - 1] <= j)
+                    take = dp[i - 1][j - arr[i - 1]];
+                boolean notTake = dp[i - 1][j];
+
+                dp[i][j] = take || notTake;
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i <= target; i++) {
+            if (dp[n - 1][i]) {
+                var absDiff = Math.abs(target - i);
+                min = Math.min(min, Math.abs(i - absDiff));
+            }
+        }
+
+        return min;
+    }
+
 }
