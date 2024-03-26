12f478f1a3f0390d28de0adbe0506f37c57ac380
manu <manukhurana10397@gmail.com>
2024-03-27 00:43:39 +0530

    dp

---
+++ src/main/java/com/example/dsa/NewCode/DP/CoinChange2.java
@@ -0,0 +1,67 @@
+package com.example.dsa.NewCode.DP;
+
+public class CoinChange2 {
+
+    public int change(int amount, int[] coins) {
+        // return changeRecurssion(coins.length - 1, amount, coins);
+
+        // int[][] dp = new int[coins.length][amount+1];
+        // return changeMemoization(coins.length - 1, amount, coins, dp);
+
+        return changeTabulation(amount, coins);
+    }
+
+    public int changeRecurssion(int ind, int amount, int[] coins) {
+        if (amount == 0)
+            return 1;
+        if (ind < 0)
+            return 0;
+
+        int take = coins[ind] > amount ? 0 : changeRecurssion(ind, amount - coins[ind], coins);
+        int notTake = changeRecurssion(ind - 1, amount, coins);
+
+        return take + notTake;
+    }
+
+    public int changeMemoization(int ind, int amount, int[] coins, int[][] dp) {
+        if (amount == 0)
+            return 1;
+        if (ind < 0)
+            return 0;
+
+        if (dp[ind][amount] != 0)
+            return dp[ind][amount];
+
+        int take = coins[ind] > amount ? 0 : changeRecurssion(ind, amount - coins[ind], coins);
+        int notTake = changeRecurssion(ind - 1, amount, coins);
+
+        dp[ind][amount] = take + notTake;
+
+        return take + notTake;
+    }
+
+    public int changeTabulation(int amount, int[] coins) {
+
+        int[][] dp = new int[coins.length + 1][amount + 1];
+
+        for (int ind = 0; ind <= coins.length; ind++)
+            dp[ind][0] = 1;
+
+        for (int ind = 1; ind <= coins.length; ind++) {
+            for (int amt = 0; amt <= amount; amt++) {
+                int notTake = dp[ind - 1][amt];
+                int take = coins[ind - 1] > amt ? 0 : dp[ind][amt - coins[ind - 1]];
+
+                dp[ind][amt] = take + notTake;
+            }
+        }
+
+        return dp[coins.length][amount];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 5 };
+        System.out.println(change(5, arr));
+
+    }
+}
--- src/main/java/com/example/dsa/NewCode/DP/TargetSum.java
+++ src/main/java/com/example/dsa/NewCode/DP/TargetSum.java
@@ -4,6 +4,62 @@ import java.util.Arrays;
 
 public class TargetSum {
 
+    public static int findWaysRecurssion(int index, int num[], int tar) {
+
+        if (tar == 0)
+            return 1;
+        if (index == 0)
+            return num[index] == tar ? 1 : 0;
+
+        // take + notTake
+        return findWaysRecurssion(index - 1, num, tar - num[index]) + findWaysRecurssion(index - 1, num, tar);
+
+    }
+
+    // Resursion + Memoization , time: O(N) , space O(2N)
+    public static int findWaysMemoization(int index, int num[], int tar, int[][] dp) {
+
+        if (tar == 0)
+            return 1;
+        if (index == 0)
+            return num[index] == tar ? 1 : 0;
+
+        if (dp[index][tar] != 0)
+            return dp[index][tar];
+
+        // take + notTake
+        int take = (num[index] > tar) ? 0 : findWaysMemoization(index - 1, num, tar - num[index], dp);
+        int notTake = findWaysMemoization(index - 1, num, tar, dp);
+
+        dp[index][tar] = take + notTake;
+
+        return take + notTake;
+    }
+
+    // Tabulation , time: O(N) , space O(N)
+    /*
+     * 1. Base case
+     * 2. took at the changing parameter and write nested loops
+     * 3. copy the recursion
+     */
+    public static int findWaysTabulation(int num[], int tar) {
+        int[][] dp = new int[num.length + 1][tar + 1];
+
+        for (int i = 0; i < num.length; i++)
+            dp[i][0] = 1;
+
+        for (int index = 1; index <= num.length; index++) {
+            for (int sum = 0; sum <= tar; sum++) {
+                int take = (num[index - 1] > sum) ? 0 : dp[index - 1][sum - num[index - 1]];
+                int notTake = dp[index - 1][sum];
+
+                dp[index][sum] = (take + notTake) % 1000000007;
+            }
+        }
+
+        return dp[num.length][tar];
+    }
+
     public static int findWaysSpaceOptimization(int num[], int tar) {
         int[] curr = new int[tar + 1];
 
