9ad0d2c85b6a1661820b81c21b1000cedc9ab74d
manu <manukhurana10397@gmail.com>
2024-03-31 16:18:27 +0530

    dp / print longest possible substring

---
+++ src/main/java/com/example/dsa/NewCode/DP/Strings/PrintLongestCommonSubsequence.java
@@ -0,0 +1,52 @@
+package com.example.dsa.NewCode.DP.Strings;
+
+public class PrintLongestCommonSubsequence {
+
+    public static int[][] lcsTabulation(String s, String t) {
+        // since the base case is (sl < 0 || tl < 0) : we dont have -ve we will shilf
+        // element by 1
+
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+
+        for (int i = 0; i <= s.length(); i++) {
+            for (int j = 0; j <= t.length(); j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+                else {
+                    if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                        dp[i][j] = 1 + dp[i - 1][j - 1];
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+        }
+
+        return dp;
+    }
+
+    public static String lcss(String s, String t) {
+        var dp = lcsTabulation(s, t);
+
+        int i = s.length();
+        int j = t.length();
+
+        StringBuilder builder = new StringBuilder();
+
+        while (i > 0 && j > 0) {
+            if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                builder.insert(0, s.charAt(i));
+                i -= 1;
+                j -= 1;
+            } else {
+                if (dp[i - 1][j] > dp[i][j - 1]) {
+                    i -= 1;
+                } else {
+                    j -= 1;
+                }
+            }
+        }
+
+        return builder.toString();
+    }
+}
