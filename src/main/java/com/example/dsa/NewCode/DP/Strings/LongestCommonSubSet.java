16f650812052e854f5802026d32e8d315b64705f
manu <manukhurana10397@gmail.com>
2024-03-31 18:40:28 +0530

    dp/ longest common subset

--- src/main/java/com/example/dsa/NewCode/DP/Strings/LongestCommonSubSet.java
+++ src/main/java/com/example/dsa/NewCode/DP/Strings/LongestCommonSubSet.java
@@ -4,12 +4,42 @@ package com.example.dsa.NewCode.DP.Strings;
 public class LongestCommonSubSet {
 
     private static int lcs(String s1, String s2) {
-        return lcsTabulation(s1, s2);
+        // return lcsRecursion(s1.length() - 1, s1, s2.length() - 1, s2);
+
+        int[][] dp = new int[s1.length()][s1.length()];
+        return lcsMemoization(s1.length() - 1, s1, s2.length() - 1, s2, dp);
+        // return lcsTabulation(s1, s2);
+        // return lcsSpaceOptimization(s1, s2);
     }
 
     // base case
     // iterate all path
     // return max;
+    private static int lcsRecursion(int l1, String s1, int l2, String s2) {
+
+        if (l1 == 0 || l2 == 0)
+            return 0;
+
+        if (s1.charAt(l1) == s2.charAt(l2))
+            return 1 + lcsRecursion(l1 - 1, s1, l2 - 1, s2);
+        return Math.max(lcsRecursion(l1 - 1, s1, l2, s2), lcsRecursion(l1, s1, l2 - 1, s2));
+    }
+
+    private static int lcsMemoization(int l1, String s1, int l2, String s2, int[][] dp) {
+
+        if (l1 == 0 || l2 == 0)
+            return 0;
+
+        if (s1.charAt(l1) == s2.charAt(l2)) {
+            int same = 1 + lcsMemoization(l1 - 1, s1, l2 - 1, s2, dp);
+            dp[l1][l2] = same;
+            return same;
+        }
+        int notSame = Math.max(lcsMemoization(l1 - 1, s1, l2, s2, dp), lcsMemoization(l1, s1, l2 - 1, s2, dp));
+        dp[l1][l2] = 0;
+        return notSame;
+    }
+
     private static int lcsTabulation(String s1, String s2) {
         int[][] dp = new int[s1.length() + 1][s2.length() + 1];
 
@@ -28,10 +58,32 @@ public class LongestCommonSubSet {
         return maxLength;
     }
 
+    private static int lcsSpaceOptimization(String s1, String s2) {
+        int[] prev = new int[s2.length() + 1];
+        int[] cur = new int[s2.length() + 1];
+
+        int maxLength = 0;
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    cur[j] = 1 + prev[j - 1];
+                    maxLength = Math.max(maxLength, cur[j]);
+                } else {
+                    cur[j] = 0;
+                }
+            }
+            prev = cur;
+            cur = new int[s2.length() + 1];
+
+        }
+
+        return maxLength;
+    }
+
     public static void main(String[] args) {
 
-        String s1 = "abcdefghij";
-        String s2 = "abzdefghij";
+        String s1 = "abcd";
+        String s2 = "abzd";
         System.out.println(lcs(s1, s2));
     }
 
