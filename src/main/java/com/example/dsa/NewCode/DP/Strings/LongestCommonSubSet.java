14c2f6b5ce6fccdcb662ee151d2f9441c6489ff8
manu <manukhurana10397@gmail.com>
2024-03-31 17:59:32 +0530

    dp/ longest common substring consecutive

--- src/main/java/com/example/dsa/NewCode/DP/Strings/LongestCommonSubSet.java
+++ src/main/java/com/example/dsa/NewCode/DP/Strings/LongestCommonSubSet.java
@@ -1,31 +1,37 @@
 package com.example.dsa.NewCode.DP.Strings;
 
-// subsequece : continus order (consequtive)
+// subsequece : continus order (consecutive)
 public class LongestCommonSubSet {
 
     private static int lcs(String s1, String s2) {
-        return lcsRecursion(s1.length() - 1, s1, s2.length() - 1, s2);
+        return lcsTabulation(s1, s2);
     }
 
     // base case
     // iterate all path
     // return max;
-    private static int lcsRecursion(int l1, String s1, int l2, String s2) {
+    private static int lcsTabulation(String s1, String s2) {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
 
-        if (l1 == 0 || l2 == 0) {
-            return 0;
+        int maxLength = 0;
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    maxLength = Math.max(maxLength, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
         }
 
-        if (s1.charAt(l1) == s2.charAt(l2))
-            return 1 + lcsRecursion(l1 - 1, s1, l2 - 1, s2);
-        return lcsRecursion(l1 - 1, s1, l2 - 1, s2);
-
+        return maxLength;
     }
 
     public static void main(String[] args) {
 
-        String s1 = "abcd";
-        String s2 = "abzd";
+        String s1 = "abcdefghij";
+        String s2 = "abzdefghij";
         System.out.println(lcs(s1, s2));
     }
 
