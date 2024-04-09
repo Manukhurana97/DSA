56be6f808a3e648c3952b5983d7e60ac121ead16
manu <manukhurana10397@gmail.com>
2024-04-09 12:48:49 +0530

    dp/minimin insertion or deletion to make a to b

--- src/main/java/com/example/dsa/NewCode/DP/Strings/MinInsertionDeletionToMakeAtoB.java
+++ src/main/java/com/example/dsa/NewCode/DP/Strings/MinInsertionDeletionToMakeAtoB.java
@@ -5,8 +5,10 @@ public class MinInsertionDeletionToMakeAtoB {
     private static int lca(String a, String b) {
         // return lcaRecursion(a.length(), b.length(), a, b);
 
-        int[][] dp = new int[a.length() + 1][b.length() + 1];
-        return lcaMemoization(a.length(), b.length(), a, b, dp);
+        // int[][] dp = new int[a.length() + 1][b.length() + 1];
+        // return lcaMemoization(a.length(), b.length(), a, b, dp);
+
+        return lcaTabulation(a, b);
     }
 
     // recursion
@@ -41,6 +43,28 @@ public class MinInsertionDeletionToMakeAtoB {
         dp[i][j] = notTake;
         return notTake;
 
+    }
+
+    // tabulation
+    private static int lcaTabulation(String a, String b) {
+
+        int[][] dp = new int[a.length() + 1][b.length() + 1];
+
+        for (int i = 0; i <= a.length(); i++) {
+            for (int j = 0; j <= b.length(); j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else {
+                    if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                        dp[i][j] = 1 + dp[i - 1][j - 1];
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+        }
+
+        return dp[a.length()][b.length()];
     }
 
     private static int canYouMake(String a, String b) {
