b4a1427cd8f6b8f13c7afd4d256e66ffce151123
manu <manukhurana10397@gmail.com>
2024-04-16 16:40:25 +0530

    long increaseing sequence

--- src/main/java/com/example/dsa/NewCode/DP/LIS6/LongestIncreasingSubSequence.java
+++ src/main/java/com/example/dsa/NewCode/DP/LIS6/LongestIncreasingSubSequence.java
@@ -18,9 +18,7 @@ public class LongestIncreasingSubSequence {
 
         // return lengthOfLISSpaceOptimization(nums);
 
-        // return lengthOfLISSpaceOptimization1(nums);
-
-        return lengthOfLISPrintSubSequence(nums);
+        return lengthOfLISSpaceOptimization1(nums);
     }
 
     private static int lengthOfLISRecurssion(int index, int prev, int[] nums) {
@@ -114,39 +112,6 @@ public class LongestIncreasingSubSequence {
         }
 
         return LIS;
-    }
-
-    // printing a path
-    private static int lengthOfLISPrintSubSequence(int nums[]) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        int[] parent = new int[n];
-
-        Arrays.fill(dp, 1);
-        Arrays.fill(parent, -1);
-
-        int maxElement = 0;
-        for (int index = 0; index < n; index++) {
-            for (int prev = 0; prev < index; prev++) {
-                if (nums[prev] < nums[index] && 1 + dp[prev] > dp[index]) {
-                    dp[index] = 1 + dp[prev];
-                    parent[index] = prev;
-                }
-            }
-            if (dp[index] > dp[maxElement])
-                maxElement = index;
-        }
-
-        List<Integer> result = new LinkedList<>(); // Changed to ArrayList for better performance
-        var index = maxElement;
-        while (maxElement != -1) {
-            result.add(0, nums[maxElement]);
-            maxElement = parent[maxElement];
-        }
-
-        System.out.println(result);
-
-        return dp[index];
     }
 
     public static void main(String[] args) {
